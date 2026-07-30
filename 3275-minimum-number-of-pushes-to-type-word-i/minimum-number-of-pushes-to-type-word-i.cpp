07cbde47b7b51d8fec0520ca24a5a4354a46424d
Pranav Shrivastava <149245722+Pranavshrivastava0611@users.noreply.github.com>
2026-07-30 11:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (54.67%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -6,8 +6,6 @@ public:
         int div = size/8;
         int rem = size%8;
         int check = (div*(div+1))/2;
-        int ans = (8*check) + (rem*(div+1));
-        return ans;
-        
+        return (8*check) + (rem*(div+1));
     }
 };
