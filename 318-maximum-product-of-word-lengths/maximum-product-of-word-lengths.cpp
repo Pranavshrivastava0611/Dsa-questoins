3fe228059c1ef17bc07a6049807f2190c68225b3
Pranav Shrivastava <149245722+Pranavshrivastava0611@users.noreply.github.com>
2026-08-15 18:03:26 +0530

    Time: 7 ms (66.67%) | Memory: 26 MB (83.30%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     int maxProduct(vector<string>& words) {
         int n = words.size();
+        vector<int> ws;
+        ws.reserve(n);
 
-        vector<unordered_map<char, bool>> vis(n);
-
-        for (int i = 0; i < n; i++) {
-            for (char c : words[i]) {
-                vis[i][c] = true;
+        for (string &word : words) {
+            int mask = 0;
+            for (char c : word) {
+                int index = c - 'a';
+                mask |= (1 << index);
             }
+
+            ws.push_back(mask);
         }
 
         int ans = 0;
 
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
+                if (ws[i] & ws[j]) continue;
 
-                bool check = false;
-
-                for (char c : words[i]) {
-                    if (vis[j].find(c) != vis[j].end()) {
-                        check = true;
-                        break;
-                    }
-                }
-
-                if (!check) {
-                    ans = max(ans, (int)(words[i].length() * words[j].length()));
-                }
+                ans = max(ans, (int) (words[i].size() * words[j].size()));
             }
         }
 
