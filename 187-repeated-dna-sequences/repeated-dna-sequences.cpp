171c6fd51022297cd4d259b5f2ec4e2bbe477e6e
Pranav Shrivastava <149245722+Pranavshrivastava0611@users.noreply.github.com>
2026-08-15 19:48:48 +0530

    Time: 783 ms (5.02%) | Memory: 282.2 MB (5.02%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -2,27 +2,43 @@ class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
         int n = s.length();
-        if(n <= 10) return {};
-        string str = "";
-        for(int i=0;i<10;i++){
-            str = str + s[i];
+        if (n <= 10) return {};
+
+        unordered_map<int, int> mp;
+        vector<string> ans;
+
+        auto getCode = [](char c) {
+            if (c == 'A') return 0;
+            if (c == 'C') return 1;
+            if (c == 'G') return 2;
+            return 3; // T
+        };
+
+        int code = 0;
+
+        // First 10 characters
+        for (int i = 0; i < 10; i++) {
+            code = (code << 2) | getCode(s[i]);
         }
-        unordered_map<string,int>store;
-        store[str]++;
-        int i=1;
-        int j=10;
-        while(j<n){
-            str.erase(str.begin());
-            str.push_back(s[j]);
-            store[str]++;
-            j++;
-        }
-        vector<string>ans;
-        for(auto p : store){
-            if(p.second >= 2){
-                ans.push_back(p.first);
+
+        mp[code]++;
+
+        // Sliding window
+        for (int i = 10; i < n; i++) {
+
+            // Remove the oldest 2 bits
+            code &= (1 << 18) - 1;
+
+            // Add the new character
+            code = (code << 2) | getCode(s[i]);
+
+            mp[code]++;
+
+            if (mp[code] == 2) {
+                ans.push_back(s.substr(i - 9, 10));
             }
         }
+
         return ans;
     }
 };
