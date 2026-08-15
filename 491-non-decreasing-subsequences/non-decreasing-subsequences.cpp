7778976d6e5e6652e4fb04ea69690b658f3d6f25
Pranav Shrivastava <149245722+Pranavshrivastava0611@users.noreply.github.com>
2026-08-15 20:14:08 +0530

    Time: 38 ms (48.28%) | Memory: 25.3 MB (94.31%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-    struct VectorHash {
-    size_t operator()(const vector<int>& v) const {
-        size_t hash = 0;
-
-        for (int x : v) {
-            hash ^= hash + x + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-        }
-        return hash;
-    }
-};
-    void solve(int index , int prev, vector<int>&nums,vector<int>&str,unordered_set<vector<int>,VectorHash>&ans){
+    void solve(int index , int prev, vector<int>&nums,vector<int>&str,set<vector<int>>&ans){
         if(index >= nums.size()){
             if(str.size() >=2 ){
                 ans.insert(str);
@@ -26,7 +16,7 @@ public:
         solve(index+1,prev,nums,str,ans);
     }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        unordered_set<vector<int>,VectorHash>ans;
+        set<vector<int>>ans;
         vector<int>str;
         solve(0,-1,nums,str,ans);
        vector<vector<int>> anss(ans.begin(), ans.end());
