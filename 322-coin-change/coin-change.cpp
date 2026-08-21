83168b18d54e177a79d43d29e6ab9031ce4a2547
Pranav Shrivastava <149245722+Pranavshrivastava0611@users.noreply.github.com>
2026-08-21 19:39:00 +0530

    Time: 66 ms (8.20%) | Memory: 49.9 MB (19.73%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,44 +1,32 @@
 class Solution {
-public:
-    // int solve(int index, vector<int>&coins,int amount,vector<vector<int>>&dp){
-    //     if(index == 0){
-    //         if(amount%coins[index]!=0){
-    //             return 100001;
-    //         }else{
-    //             return amount/coins[index];
-    //         }
-    //     }
-    //     if(dp[index][amount]!=-1) return dp[index][amount];
-    //     int ans = 100001;
-    //     if(coins[index] <= amount){
-    //         ans = min(ans,1+ solve(index,coins,amount - coins[index],dp));
-    //     }
-    //     ans = min(ans,solve(index-1,coins,amount,dp));
-    //     return dp[index][amount] = ans;
-    // }
+public:     
+    int solve(int index,int sum ,vector<int>&coins,vector<vector<int>>&dp){
+        if(index==coins.size()){
+            return INT_MAX;
+        }
+        if(dp[index][sum]!=100001) return dp[index][sum];
+        if(sum < 0){
+            return INT_MAX;
+        }
+        if(sum==0) return 0;
+
+        int take = INT_MAX;
+        if(sum >=coins[index]){
+            take = min(take,solve(index,sum-coins[index],coins,dp));
+            if(take!=INT_MAX){
+                take = 1+take;
+            }
+        }
+        int ntake = solve(index+1,sum,coins,dp);
+        return dp[index][sum] = min(take,ntake);
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        for(int i=0;i<=amount;i++){
-             if(i%coins[0]!=0){
-                dp[0][i] = 100001;
-            }else{
-                dp[0][i] =  i/coins[0];
-            }
-        }
-        for(int i=1;i<coins.size();i++){
-            for(int j=0;j<=amount;j++){
-                int ans = 100001;
-                if(coins[i] <= j){
-
-                ans = min(ans,1+ dp[i][j-coins[i]]);
-
-                 }
-                ans  = min(ans,dp[i-1][j]);
-
-                dp[i][j] = ans;
-            }
-        }
-        return dp[n-1][amount]==100001 ? -1 : dp[n-1][amount];
+        vector<vector<int>>dp(n,vector<int>(amount+1,100001));
+        int sum =0;
+        int ans = solve(0,amount,coins,dp);
+        cout<<ans;
+        if(ans==INT_MAX) return -1;
+        return ans;
     }
 };
