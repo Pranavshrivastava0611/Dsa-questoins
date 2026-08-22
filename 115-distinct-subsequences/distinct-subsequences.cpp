463040bca3cebc9f114d5570fc9f3a44ac9e586d
Pranav Shrivastava <149245722+Pranavshrivastava0611@users.noreply.github.com>
2026-08-22 20:18:21 +0530

    Time: 452 ms (8.70%) | Memory: 32.1 MB (44.62%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    unordered_map<char,bool>store;
+     unordered_map<string,int>strr;
+    int solve(int index,int &length,string &str,string &s,string &t,vector<vector<int>>&dp){
+        if(str==t){
+            return 1;
+        }
+        if(index==length){
+            return str==t;
+        }
+        int idx = strr[str];
+        if(dp[index][idx]!=-1) return dp[index][idx];
+        
+        str.push_back(s[index]);
+        int take = 0;
+        if(strr.find(str)!=strr.end()){
+            take = solve(index+1,length,str,s,t,dp);
+        }
+        str.pop_back();
+        int ntake = solve(index+1,length,str,s,t,dp);
+
+        return dp[index][idx] = take + ntake;
+
+    }
+    int numDistinct(string s, string t) {
+        for(auto c : t){
+            store[c] = true;
+        }
+        int length = s.length();
+        string check = "";
+        int length2 = t.length();
+        strr[""] = 0;
+        for(int i=0;i<length2;i++){
+            check = check + t[i];
+            strr[check] = i+1;
+        }
+        vector<vector<int>>dp(length+1,vector<int>(length2+1,-1));
+        string str ="";
+        int ans = solve(0,length,str,s,t,dp);
+        return ans;
+    }
+};
