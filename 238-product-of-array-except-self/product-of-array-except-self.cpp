416011e91b93bce869be4f2c90aee863084c9ef5
Pranav Shrivastava <149245722+Pranavshrivastava0611@users.noreply.github.com>
2026-09-17 16:27:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.78%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -6,17 +6,14 @@ public:
         vector<int>suf(n,1);
         int p = 1;
         int s = 1;
-        for(int i=0;i<n;i++){
-            pre[i] = p;
-            p = p*nums[i];
-        }
         for(int i=n-1;i>=0;i--){
             suf[i] = s;
             s = s *nums[i];
         }
         vector<int>ans(n);
         for(int i=0;i<n;i++){
-            ans[i] = pre[i]*suf[i];
+            ans[i] = p*suf[i];
+            p = p*nums[i];
         }
         return ans;
     }
